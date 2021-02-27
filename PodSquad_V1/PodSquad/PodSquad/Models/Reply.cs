69308d4f917d6545e77996f2978b07232afb9f20
Jecca Arthur <arthurj@my.lanecc.edu>
2021-02-26 19:51:00 -0800

    added Reply model

--- PodSquad_V1/PodSquad/PodSquad/Models/Reply.cs
+++ PodSquad_V1/PodSquad/PodSquad/Models/Reply.cs
@@ -3,8 +3,11 @@ namespace PodSquad.Models
 {
     public class Reply
     {
-        public Reply()
-        {
-        }
+        // TODO: uncomment properties when other models have been built
+
+        public int ReplyID { get; set; }
+        //public AppUser Responder { get; set; }
+        public DateTime Date { get; set; }
+        public string ReplyText { get; set; }
     }
 }
