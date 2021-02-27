3d08b14a29df6deda892ddc24a29ad31417a2954
Jecca Arthur <arthurj@my.lanecc.edu>
2021-02-26 20:09:46 -0800

    added DbContext model with DbSets

---
+++ PodSquad_V1/PodSquad/PodSquad/Models/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace PodSquad.Models
+{
+    public class DbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        public DbSet<Podcast> Podcasts { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+    }
+}
