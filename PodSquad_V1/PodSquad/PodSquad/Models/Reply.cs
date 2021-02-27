656863461ddb0b86067ca80407bfbf495667f9b1
Jecca Arthur <arthurj@my.lanecc.edu>
2021-02-26 21:07:21 -0800

    uncommented AppUser property now that class has been built

--- PodSquad_V1/PodSquad/PodSquad/Models/Reply.cs
+++ PodSquad_V1/PodSquad/PodSquad/Models/Reply.cs
@@ -3,10 +3,8 @@ namespace PodSquad.Models
 {
     public class Reply
     {
-        // TODO: uncomment properties when other models have been built
-
         public int ReplyID { get; set; }
-        //public AppUser Responder { get; set; }
+        public AppUser Responder { get; set; }
         public DateTime Date { get; set; }
         public string ReplyText { get; set; }
     }
