13c82c14aa8dc72ab998b4f34971b3d9d662dc55
Jecca Arthur <arthurj@my.lanecc.edu>
2021-02-26 19:50:43 -0800

    added Post model

--- PodSquad_V1/PodSquad/PodSquad/Models/Post.cs
+++ PodSquad_V1/PodSquad/PodSquad/Models/Post.cs
@@ -3,8 +3,15 @@ namespace PodSquad.Models
 {
     public class Post
     {
-        public Post()
-        {
-        }
+        // TODO: uncomment properties when other models have been built
+
+        //private List<Reply> replies = new List<Reply>();
+
+        public int PostID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+        //public AppUser Poster { get; set; }
+        //public List<Reply> Replies { get { return replies; } }
     }
 }
