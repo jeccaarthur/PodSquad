61276b9e325a91d13ad189d3cc32e48efcdea055
Jecca Arthur <arthurj@my.lanecc.edu>
2021-02-26 19:30:37 -0800

    added Podcast model

---
+++ PodSquad_V1/PodSquad/PodSquad/Models/Podcast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace PodSquad.Models
+{
+    public class Podcast
+    {
+        // TODO: uncomment properties when other models have been built
+
+        // private List<Review> reviews = new List<Review>();
+
+        public int PodcastID { get; set; }
+        public string Name { get; set; }
+        // public Genre Genre { get; set; }
+        public string Network { get; set; }
+        public string HostName { get; set; }
+
+        // public List<Review> Reviews { get { return reviews; } }
+    }
+}
