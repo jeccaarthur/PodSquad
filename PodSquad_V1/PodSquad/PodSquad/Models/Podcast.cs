f18839f00fc1c971475119b5c1369808de424683
Jecca Arthur <arthurj@my.lanecc.edu>
2021-02-26 19:54:04 -0800

    finished Podcast properties

--- PodSquad_V1/PodSquad/PodSquad/Models/Podcast.cs
+++ PodSquad_V1/PodSquad/PodSquad/Models/Podcast.cs
@@ -1,18 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace PodSquad.Models
 {
     public class Podcast
     {
-        // TODO: uncomment properties when other models have been built
-
-        // private List<Review> reviews = new List<Review>();
+        private List<Review> reviews = new List<Review>();
 
         public int PodcastID { get; set; }
         public string Name { get; set; }
-        // public Genre Genre { get; set; }
+        public Genre Genre { get; set; }
         public string Network { get; set; }
         public string HostName { get; set; }
-
-        // public List<Review> Reviews { get { return reviews; } }
+        public List<Review> Reviews { get { return reviews; } }
     }
 }
