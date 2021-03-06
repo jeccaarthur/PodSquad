f37bfd84ea3438deb5b8942c19f244a7bf8e27b6
Jecca Arthur <arthurj@my.lanecc.edu>
2021-03-05 18:42:23 -0800

    added RegisterVM with properties and validation

---
+++ PodSquad_V1/PodSquad/PodSquad/Models/RegisterVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PodSquad.Models
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Please enter your first name.")]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name.")]
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
