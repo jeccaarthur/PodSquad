0a14feeef741ea75569eb5120da66a5108c24275
Jecca Arthur <arthurj@my.lanecc.edu>
2021-03-05 20:16:38 -0800

    added shell controller and views for Podcast

---
+++ PodSquad_V1/PodSquad/PodSquad/Controllers/PodcastController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PodSquad.Models;
+
+namespace PodSquad.Controllers
+{
+    public class PodcastController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
