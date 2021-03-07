fa0eb87486bf61a565f064f78b6bdc9d13f66a5a
Jecca Arthur <arthurj@my.lanecc.edu>
2021-03-06 20:39:07 -0800

    added description property

--- PodSquad_V1/PodSquad/PodSquad/Models/Podcast.cs
+++ PodSquad_V1/PodSquad/PodSquad/Models/Podcast.cs
@@ -12,6 +12,10 @@ namespace PodSquad.Models
         public Genre Genre { get; set; }
         public string Network { get; set; }
         public string HostName { get; set; }
+        public string Description { get; set; }
         public List<Review> Reviews { get { return reviews; } }
+
+        // TODO: add average rating property with logic to calculate
+        // public double AvgRating { get; set; }
     }
 }
