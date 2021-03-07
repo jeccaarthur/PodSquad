f2d38b8693d5ab01fd9f1e9ab59b842a88c6f4d5
Jecca Arthur <arthurj@my.lanecc.edu>
2021-03-07 12:36:22 -0800

    renamed context model and added alternate key to Genre model so genre names will be unique

---
+++ PodSquad_V1/PodSquad/PodSquad/Models/PodContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PodSquad.Models
+{
+    public class PodContext : IdentityDbContext
+    {
+        public PodContext(DbContextOptions<PodContext> options) : base(options) { }
+
+        public DbSet<Podcast> Podcasts { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        //public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);  // get around primary key error on IdentityUser class
+
+            builder.Entity<Genre>()
+                .HasAlternateKey(g => g.Name);
+        }
+    }
+}
