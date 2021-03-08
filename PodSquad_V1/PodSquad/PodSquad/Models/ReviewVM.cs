54761d7551c61024422f1e4faed3f14efbf4b362
Jecca Arthur <arthurj@my.lanecc.edu>
2021-03-08 14:57:54 -0800

    added ReviewVM with properties - still needs validation

---
+++ PodSquad_V1/PodSquad/PodSquad/Models/ReviewVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PodSquad.Models
+{
+    public class ReviewVM
+    {
+        // TODO: add validation to fields
+
+        public int PodcastID { get; set; }
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+    }
+}
