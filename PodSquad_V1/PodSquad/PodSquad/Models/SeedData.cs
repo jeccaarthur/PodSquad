0de1f06a6b1816fea860b81e7115ce4d01b6a722
Jecca Arthur <arthurj@my.lanecc.edu>
2021-03-16 16:52:30 -0700

    seeded db with initial podcasts, forum post, and admin user

--- PodSquad_V1/PodSquad/PodSquad/Models/SeedData.cs
+++ PodSquad_V1/PodSquad/PodSquad/Models/SeedData.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using PodSquad.Repositories;
 
@@ -13,9 +14,6 @@ namespace PodSquad.Models
     {
         public static void Seed(PodContext context, RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager, IPodcastRepository repo)
         {
-            // TODO: update seeded data to reflect updated podcast properties
-            // (removed genre and hostname)
-            
             // if podcast table is empty, seed data
             if (!context.Podcasts.Any())
             {
@@ -24,16 +22,19 @@ namespace PodSquad.Models
                 result = roleManager.CreateAsync(new IdentityRole("Admin")).Result;
 
                 // seed a default administrator
-                AppUser siteadmin = new AppUser
+                AppUser admin = new AppUser
                 {
-                    UserName = "siteadmin",
+                    UserName = "admin",
                     FirstName = "Site",
                     LastName = "Admin"
                 };
 
-                userManager.CreateAsync(siteadmin, "Secret-123").Wait();
-                IdentityRole adminRole = roleManager.FindByNameAsync("Admin").Result;
-                userManager.AddToRoleAsync(siteadmin, adminRole.Name);
+                userManager.CreateAsync(admin, "Secret-123").Wait();    // creates user in db
+                IdentityRole adminRole = roleManager.FindByNameAsync("Admin").Result;   
+                userManager.AddToRoleAsync(admin, adminRole.Name);      // adds user to admin role
+
+                context.SaveChanges();
+
 
                 // seed first forum post
                 var firstPost = new Post
@@ -41,10 +42,12 @@ namespace PodSquad.Models
                     Title = "Welcome to the PodSquad",
                     Body = "Hello there! I hope you enjoy your time here finding new podcasts to enjoy and discussing them with fellow squad members. Drop me a line if you run into issues!",
                     Date = DateTime.Now,
-                    Poster = siteadmin
+                    Poster = new AppUser { UserName = "siteadmin" }
                 };
 
                 context.Posts.Add(firstPost);
+                context.SaveChanges();
+
 
                 // seed spotify podcasts
                 string token = repo.GetAccessToken().Result;
@@ -52,19 +55,21 @@ namespace PodSquad.Models
                 // my favorite murder 
                 Podcast myFavMurder = repo.GetSpotifyPodcast(token, "my favorite murder").Result;
                 context.Podcasts.Add(myFavMurder);
+                context.SaveChanges();
 
                 // higher learning
                 Podcast higherLearning = repo.GetSpotifyPodcast(token, "higher learning").Result;
                 context.Podcasts.Add(higherLearning);
+                context.SaveChanges();
 
                 // your own backyard
                 Podcast yourOwnBackyard = repo.GetSpotifyPodcast(token, "your own backyard").Result;
                 context.Podcasts.Add(yourOwnBackyard);
+                context.SaveChanges();
 
                 // hidden brain
                 Podcast hiddenBrain = repo.GetSpotifyPodcast(token, "hidden brain").Result;
                 context.Podcasts.Add(hiddenBrain);
-
                 context.SaveChanges();
             }
         }
